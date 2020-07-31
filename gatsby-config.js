e022ac2a3fcf8a110ee23973a89398a4ea3b21b5
Anelise Newman <apnewman@mit.edu>
2020-07-30 21:03:21 -0700

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,6 +42,12 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-174136095-1",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
