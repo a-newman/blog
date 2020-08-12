32519f106f92caec63e0ceef72d0a483c2ab4b12
Anelise Newman <apnewman@mit.edu>
2020-08-12 13:24:41 -0700

    update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        icon: `src/images/favicon.png`,
+        icon: `src/images/favicon_white_bg.png`,
       },
     },
     {
