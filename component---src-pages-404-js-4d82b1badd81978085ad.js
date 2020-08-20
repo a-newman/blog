405e8fcd441dca78218a33569a2d129343705b0a
Anelise Newman <apnewman@mit.edu>
2020-08-19 22:49:05 -0700

    Updates

---
+++ component---src-pages-404-js-4d82b1badd81978085ad.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),o=n("Bl7J"),r=n("vrFN");t.default=function(){return l.a.createElement(o.a,null,l.a.createElement(r.a,{title:"Page not found"}),l.a.createElement("div",{class:"shadowBox"},l.a.createElement("h1",null,"404: Page not found"),l.a.createElement("p",null,"Uh-oh...looks like we've got some loose threads here.")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-4d82b1badd81978085ad.js.map
